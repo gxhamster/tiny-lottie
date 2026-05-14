e156035f731c0a948a14cd4b74654d8644d9b8c8
Iyaan Azeez <iyaanazeez757@gmail.com>
2026-05-14 15:32:43 +0500

    combine individual build scripts into build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,31 @@
-if [[ $1 == "debug" ]]
-then
-    shift
+mkdir build
 
-	odin build src/ -show-timings -collection:src=src -out:build/lottie_validator -o:none -debug
-    exit 0
+
+if [[ $1 == "bench" ]]; then
+    shift
+    if [[ $1 == "debug" ]]; then
+        odin build tools/benchmark/ -show-timings -collection:src=src -out:build/benchmark -o:none -debug
+    else
+        odin build tools/benchmark/ -show-timings -collection:src=src -out:build/benchmark -o:speed
+    fi
 fi
 
-odin build tools/lottie_validator/main.odin -file -show-timings -collection:src=src -out:build/lottie_validator -o:speed
+if [[ $1 == "validator" ]]; then
+    shift
+    if [[ $1 == "debug" ]]; then
+        odin build tools/lottie_validator/main.odin -file -show-timings -collection:src=src -out:build/lottie_validator -o:none -debug
+    else
+        odin build tools/lottie_validator/main.odin -file -show-timings -collection:src=src -out:build/lottie_validator -o:speed
+    fi
+fi
+
+if [[ $1 == "validator" ]]; then
+    shift
+    if [[ $1 == "debug" ]]; then
+        odin build tools/cubic_curve_generator/main.odin -file -show-timings -collection:src=src -out:build/cubic_curve_gen -o:none -debug
+    else
+        odin build tools/cubic_curve_generator/main.odin -file -show-timings -collection:src=src -out:build/cubic_curve_generator -o:speed
+    fi
+fi
+
+
