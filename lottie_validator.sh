3c254cf94ac4ced4f1b5580f2fe4b3d5f7777261
Iyaan Azeez <iyaanazeez757@gmail.com>
2026-05-14 09:25:41 +0500

    Move sub build scripts into build.sh

--- lottie_validator.sh
+++
@@ -1,9 +0,0 @@
-if [[ $1 == "debug" ]]
-then
-    shift
-
-	odin build tools/lottie_validator/main.odin -file -show-timings -collection:src=src -out:build/lottie_validator -o:none -debug
-    exit 0
-fi
-
-odin build tools/lottie_validator/main.odin -file -show-timings -collection:src=src -out:build/lottie_validator -o:speed
